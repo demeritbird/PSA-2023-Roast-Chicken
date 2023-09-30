2cd98499139c08987d0be3606c9de424a81f4fae
demeritbird <lekjiewei@hotmail.sg>
2023-09-30 15:58:52 +0800

    feat: init summary-block component

--- client/src/layouts/Dashboard/DashboardLayout.jsx
+++ client/src/layouts/Dashboard/DashboardLayout.jsx
@@ -1,7 +1,19 @@
 import styles from './DashboardLayout.module.scss';
+import SummaryBlock from './SummaryBlock';
 
 function DashboardLayout() {
-  return <div>DashboardLayout</div>;
+  return (
+    <div className={styles.bound}>
+      <div className={styles.bound__left}>
+        <div className={styles['scroll-layout__mask']}>
+          <div className={styles['scroll-layout__body']}>
+            <SummaryBlock />
+          </div>
+        </div>
+      </div>
+      <div className={styles.bound__right}></div>
+    </div>
+  );
 }
 
 export default DashboardLayout;
